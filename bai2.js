ed9fc5325592a7af495b22457df45ea6ddeca77d
minh chau <chaugriezmann963@gmail.com>
2023-09-14 12:48:52 +0700

    first

---
+++ bai2.js
@@ -0,0 +1,5 @@
+let radius = prompt("bán kính: ");
+let c = 2 * radius * 3.14;
+document.write("chu vi hình tròn là: "+ c);
+let s = radius * radius * 3.14;
+document.write("diện tích hình tròn là: "+ s);
